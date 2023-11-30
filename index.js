ba36a9311c91b562fc97ba0507530ac0cc50bb00
Mukeshkhanna11 <mukeshmass.9865@gmail.com>
2023-11-30 15:18:37 +0530

    updated  date-fns

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-console.log("testing");
-console.log("complete");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+console.log(format(new Date(), "yyyMMdd\tHH:mm:ss"));
+
+console.log(uuid());
